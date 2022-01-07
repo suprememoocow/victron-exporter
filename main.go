37b0c8136ae9854a0de3241b84c05c4a4ddeb277
Finwë <finwe@finwe.tf>
2022-01-07 14:08:31 +0100

    add support for ENV VAR

--- main.go
+++ main.go
@@ -12,19 +12,45 @@ import (
 
 var systemSerialID = ""
 
+var (
+	listenAddress = flag.String("web.listen-address",
+		getEnv("LISTEN_ADDR", "127.0.0.1:9226"),
+		"Address on which to expose metrics and web interface.")
+
+	clientPrefix = flag.String("mqtt.client_prefix",
+		getEnv("MQTT_CLIENT_PREFIX", "victron_exporter"),
+		"Prefix for MQTT clientID")
+
+	pollInterval = flag.Duration("victron.poll_interval",
+		getDurationEnv("VICTRON_POLL_INTERVAL", 10*time.Second),
+		"CGX MQTT poll interval")
+
+	host = flag.String("mqtt.host",
+		getEnv("MQTT_HOST", ""),
+		"CGX IP address or hostname")
+
+	port = flag.Int("mqtt.port",
+		getIntEnv("MQTT_PORT", 8883),
+		"CGX MQTT PORT")
+
+	secure = flag.Bool("mqtt.secure",
+		getBoolEnv("MQTT_SECURE", true),
+		"CGX SSL-enabled communication")
+
+	username = flag.String("mqtt.username",
+		getEnv("MQTT_USERNAME", ""),
+		"Victron MQTT Cloud Username")
+
+	password = flag.String("mqtt.password",
+		getEnv("MQTT_PASSWORD", ""),
+		"Victron MQTT Cloud Password")
+
+	logLevel = flag.Int("log.level",
+		getIntEnv("LOG_LEVEL", 2),
+		"Log level: 0=debug, 1=info, 2=warn, 3=error")
+)
+
 func main() {
-	var (
-		listenAddress = flag.String("web.listen-address", ":9226",
-			"Address on which to expose metrics and web interface.")
-		host         = flag.String("mqtt.host", "", "")
-		port         = flag.Int("mqtt.port", 8883, "")
-		secure       = flag.Bool("mqtt.secure", true, "")
-		clientPrefix = flag.String("mqtt.client_prefix", "victron_exporter", "Prefix for MQTT clientID")
-		username     = flag.String("mqtt.username", "", "")
-		password     = flag.String("mqtt.password", "", "")
-		pollInterval = flag.Duration("victron.poll_interval", 10*time.Second, "MQTT poll interval")
-		logLevel     = flag.Int("log.level", 2, "Log level: 0=debug, 1=info, 2=warn, 3=error")
-	)
 	flag.Parse()
 
 	setLogLevel(*logLevel)
